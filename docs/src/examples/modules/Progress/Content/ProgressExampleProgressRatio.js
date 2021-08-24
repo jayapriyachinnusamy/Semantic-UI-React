6dc47d551d31b46b8a3d1f410e6489a14857a82f
jayapriyachinnusamy <89445627+jayapriyachinnusamy@users.noreply.github.com>
2021-08-24 12:40:33 +0530

    docs(ProgressExampleProgressRatio): your description

--- docs/src/examples/modules/Progress/Content/ProgressExampleProgressRatio.js
+++ docs/src/examples/modules/Progress/Content/ProgressExampleProgressRatio.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Progress } from 'semantic-ui-react'
 
 const ProgressExampleProgressRatio = () => (
-  <Progress value='3' total='5' progress='ratio' />
+  <Progress value='3' total='10' progress='ratio' />
 )
 
 export default ProgressExampleProgressRatio
